b7360d3148e639ca5a448dfb269c7a7a61b7156d
medev-fsai <256591220+medev-fsai@users.noreply.github.com>
2026-04-09 12:34:46 +0100

    Get computer choice logic.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello world !");
+
+function getComputerChoice(){
+    //randomly returns "rock", "paper", "scissors"
+    //HOW TO? Using Math.random method. It is a uniform distribution over the interval [0, 1].
+    //Numbers in [0, 1/3], [1/3, 2/3] and [2/3, 1] are output with the same probability.
+    //Pseudo code:
+    //Get number between 0 and 1.
+    let guess = Math.random();
+    let computerChoice = (guess < 1/3) ? "rock": (guess < 2/3) ? "paper" : "scissors";
+    return computerChoice;
+};
+
+console.log(getComputerChoice());
