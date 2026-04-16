58e55994f3dbc178fe8c21aa3e37bdcd9c415f20
medev-fsai <256591220+medev-fsai@users.noreply.github.com>
2026-04-16 13:06:59 +0100

    Fix new game logic.

--- script.js
+++ script.js
@@ -36,47 +36,102 @@ function playRound(computerChoice, humanChoice){
         switch (humanChoice){
             case "paper":
                 humanRoundScore++;
-                console.log(`You win: computer choice ${computerChoice} vs yours ${humanChoice}.\nSCORE: Computer ${computerRoundScore}:${humanRoundScore} You`);
+                //console.log(`You win: computer choice ${computerChoice} vs yours ${humanChoice}.\nSCORE: Computer ${computerRoundScore}:${humanRoundScore} You`);
                 break;
             case "scissors":
                 computerRoundScore++;
-                console.log(`You loose: computer choice ${computerChoice} vs yours ${humanChoice}.\nSCORE: Computer ${computerRoundScore}:${humanRoundScore} You`);
+                //console.log(`You loose: computer choice ${computerChoice} vs yours ${humanChoice}.\nSCORE: Computer ${computerRoundScore}:${humanRoundScore} You`);
                 break;
             case "rock":
-                console.log(`You choose what the computer did!\nSCORE: Computer ${computerRoundScore}:${humanRoundScore} You`);
+                //console.log(`You choose what the computer did!\nSCORE: Computer ${computerRoundScore}:${humanRoundScore} You`);
                 break;
         }
     }else if(computerChoice === "paper"){
         switch (humanChoice){
             case "scissors":
                 humanRoundScore++;
-                console.log(`You win: computer choice ${computerChoice} vs yours ${humanChoice}.\nSCORE: Computer ${computerRoundScore}:${humanRoundScore} You`);
+                //console.log(`You win: computer choice ${computerChoice} vs yours ${humanChoice}.\nSCORE: Computer ${computerRoundScore}:${humanRoundScore} You`);
                 break;
             case "rock":
                 computerRoundScore++;
-                console.log(`You loose: computer choice ${computerChoice} vs yours ${humanChoice}.\nSCORE: Computer ${computerRoundScore}:${humanRoundScore} You`);
+                //console.log(`You loose: computer choice ${computerChoice} vs yours ${humanChoice}.\nSCORE: Computer ${computerRoundScore}:${humanRoundScore} You`);
                 break;
             case "paper":
-                console.log(`You choose what the computer did!\nSCORE: Computer ${computerRoundScore}:${humanRoundScore} You`);
+                //console.log(`You choose what the computer did!\nSCORE: Computer ${computerRoundScore}:${humanRoundScore} You`);
                 break;
         }
     }else{
         switch (humanChoice){
             case "rock":
                 humanRoundScore++;
-                console.log(`You win: computer choice ${computerChoice} vs yours ${humanChoice}.\nSCORE: Computer ${computerRoundScore}:${humanRoundScore} You`);
+                //console.log(`You win: computer choice ${computerChoice} vs yours ${humanChoice}.\nSCORE: Computer ${computerRoundScore}:${humanRoundScore} You`);
                 break;
             case "paper":
                 computerRoundScore++;
-                console.log(`You loose: computer choice ${computerChoice} vs yours ${humanChoice}.\nSCORE: Computer ${computerRoundScore}:${humanRoundScore} You`);
+                //console.log(`You loose: computer choice ${computerChoice} vs yours ${humanChoice}.\nSCORE: Computer ${computerRoundScore}:${humanRoundScore} You`);
                 break;
             case "scissors":
-                console.log(`You choose what the computer did!\nSCORE: Computer ${computerRoundScore}:${humanRoundScore} You`);
+                //console.log(`You choose what the computer did!\nSCORE: Computer ${computerRoundScore}:${humanRoundScore} You`);
                 break;
         }
     }
     return (computerRoundScore > humanRoundScore) ? "computer" : 
            (computerRoundScore == humanRoundScore) ? "both" : "human";
 };
+
+let computerScore = 0, humanScore = 0;
+let round = 1;
+
+const scoreDisplay = document.querySelector(".score");
+const winner = document.querySelector(".winner");
+//scoreDisplay.addEventListener("roundFinished", showRoundScore);
+
+let buttonsList = document.querySelectorAll("button");
+buttonsList.forEach((item) => {
+    item.addEventListener("click", () => {
+        
+        const humanChoice = item.id;
+        const computerChoice = getComputerChoice();
+        const roundWinner = playRound(computerChoice, humanChoice);
+        //console.log(roundWinner) 
+        switch (roundWinner){
+            case "computer":
+                computerScore++;
+                break;
+            case "human":
+                humanScore++;
+                break;
+            case "both":
+                break;
+        }
+        showRoundScore(scoreDisplay);
+        const maxScore = getMaxScore(computerScore, humanScore);
+        if (maxScore === 5){
+            displayWinner(winner);
+            initGame();
+        }
+         
+    });
+});
+
+function getMaxScore(firstScore, secondScore){
+    return firstScore > secondScore ? firstScore : secondScore;
+}
+
+function showRoundScore(item){
+    item.textContent = `SCORE: Computer ${computerScore} : ${humanScore} You`;
+    return;
+}   
+function displayWinner(item){
+    let gameWinner = computerScore > humanScore ? "computer" : "you";
+    item.textContent = `Game winner: ${gameWinner}`;
+    return;
+};
+
+function initGame() {
+    computerScore = 0;
+    humanScore = 0;
+    round = 1;
+}
 
 
